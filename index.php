d9362bda1381486e60b7bb17114f21d0e2cfc1ed
Bobbykelso <aurelien-barth_student2021@wilder.school>
2021-03-17 16:04:22 +0100

    add style.scss body button variable.scss

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Voici le CV de Hulk">
     <title>CV de Hulk</title>
-    <link rel="stylsheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <?php include("header.php");?>
