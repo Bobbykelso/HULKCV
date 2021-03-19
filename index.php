c3d870d7ccc080f6f1b5ab41449a5ffd213abb6e
Le Pober <adrien.le.pober@gmail.com>
2021-03-19 16:57:05 +0100

    Mise a jour du cv

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Voici le CV de Hulk">
     <title>CV de Hulk</title>
-    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="public/iconmoon.css">
     <link rel="stylesheet" href="public/uicons-regular-rounded.css">
 </head>
@@ -20,7 +20,7 @@
     <footer>
         <?php include("footer.php");?>
     </footer>
-    <script src="displayContent.js"></script>
+    <script src="/Javascript/displayContent.js"></script>
     <script src="/Javascript/responsiveNav.js"></script>
 </body>
 </html>
