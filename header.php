eaf9bb8bb84edad9d24f5036ac19efb398ebadcf
Bobbykelso <aurelien-barth_student2021@wilder.school>
2021-03-19 14:12:32 +0100

    change header and css

--- header.php
+++ header.php
@@ -3,11 +3,12 @@
         <div class="header">
             <a href="#default" class="logo">HulkLogo</a>
             <div class="header-right">
-                <a class="active" href="#home">Home</a>
+                <a class="active" href="#home">Accueil</a>
                 <a href="#profil">Mon Profil</a>
                 <a href="#skill">Mes Compétences</a>
                 <a href="#contact">Contact</a>
                 <a href="#about">About</a>
             </div>
-            <script src="/Javascript/responsiveNav.js"></script>
-</body>
+        </div>
+
+
