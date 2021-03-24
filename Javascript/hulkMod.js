98569f132b1f7a54f9233d86e015727080384731
Bobbykelso <aurelien-barth_student2021@wilder.school>
2021-03-24 14:53:22 +0100

    change js and scss

--- Javascript/hulkMod.js
+++ Javascript/hulkMod.js
@@ -1,17 +1,28 @@
 hulkButton = document.querySelector('#activate-hulk-mod');
 body = document.querySelector('body');
-hulkForm= document.querySelector('#hulkForm');
+hulkForm = document.querySelector('#hulkForm');
+textHuman = document.querySelectorAll('.textHuman');
+textHulk = document.querySelectorAll('.textHulk');
 
-hulkButton.addEventListener('click', function(e) {
+
+hulkButton.addEventListener('click', function (e) {
     e.preventDefault();
     body.classList.toggle('hulk-mod');
-    if(body.className){
+    if (body.className) {
         hulkButton.textContent = "AU SECOURS";
-        hulkForm.style.display="flex";
+        hulkForm.style.display = "flex";
+        for(i=0; i<textHuman.length; i++) {
+            textHuman[i].style.display = "none";
+            textHulk[i].style.display = "block";
+        };
     } else {
         hulkButton.textContent = "En savoir plus";
-        hulkForm.style.display="none";
+        hulkForm.style.display = "none";
+        for(i=0; i<textHulk.length; i++) {
+            textHuman[i].style.display = "block";
+            textHulk[i].style.display = "none";
+        };
     }
     //hulkForm.classList.toggle('display');
-    
+
 });
--- cv.php
+++ cv.php
@@ -2,31 +2,46 @@
     <article class="article">
         <h1 class="timeline" id="timeline"><i class="fi-rr-angle-down" id="icon1"></i>Timeline</h1>
         <div class="timeline__content" id="timeline__content">
+            <div class="textHuman">
+                <p>Je m'appelle Robert Bruce Banner, mais tous le monde m'appel Bruce ou Dr.Banner, je fais 1m76 pour 62kg, ancien membre du Shied & des Vengeurs, je suis diplomé en physique nucléaire, spécialisé dans les rayons Gamma. Je suis avant tout un savant timide qui cache en lui un monstre ravageur à l'esprit d'enfant, qui demande uniquement qu'on le laisse en paix...
+                </p>
+            </div>
+            <div class="textHulk">
+                blbl
+            </div>
 
-            <p>Je m'appelle Robert Bruce Banner, mais tous le monde m'appel Bruce ou Dr.Banner, je fais 1m76 pour 62kg, ancien membre du Shied & des Vengeurs, je suis diplomé en physique nucléaire, spécialisé dans les rayons Gamma. Je suis avant tout un savant timide qui cache en lui un monstre ravageur à l'esprit d'enfant, qui demande uniquement qu'on le laisse en paix...
-          </p>
         </div>
     </article>
     <article class="article">
         <h1 class="experience" id="experience"><i class="fi-rr-angle-down" id="icon2"></i>Experiences</h1>
         <div class="experience__content" id="experience__content">
-            -Création d'une bombe gamma pour les militaires.<br>
-            -Création d'un téléporteur.<br>
-            -Création d'un champ de force protecteur.<br>
+            <div class="textHuman">
+                -Création d'une bombe gamma pour les militaires.<br>
+                -Création d'un téléporteur.<br>
+                -Création d'un champ de force protecteur.<br>
+            </div>
+            <div class="textHulk">
+                blbl
+            </div>
         </div>
     </article>
     <article class="article">
         <h1 class="skills" id="skills"><i class="fi-rr-angle-down" id="icon3"></i>Skills</h1>
         <div class="skills__content" id="skills__content">
-            Détient une expertise dans divers domaines:
-            <ul>
-                <li>Biologie</li>
-                <li>Chimie</li>
-                <li>Ingénierie</li>
-                <li>Physiologie</li>
-                <li>Physique nucléaire</li>
-            </ul>
-            Capable de dissimuler avec aisance sa véritable identité.
+            <div class="textHuman">
+                Détient une expertise dans divers domaines:
+                <ul>
+                    <li>Biologie</li>
+                    <li>Chimie</li>
+                    <li>Ingénierie</li>
+                    <li>Physiologie</li>
+                    <li>Physique nucléaire</li>
+                </ul>
+                Capable de dissimuler avec aisance sa véritable identité.
+            </div>
+            <div class="textHulk">
+                blbl
+            </div>
         </div>
     </article>
     <button><a href="#profil" id="activate-hulk-mod">En savoir plus</a></button>
