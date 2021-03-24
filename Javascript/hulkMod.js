f9470857007a031c64b170ba47452cbc0df4ba6f
Bobbykelso <aurelien-barth_student2021@wilder.school>
2021-03-24 14:11:10 +0100

    add hulkform on formulaire and js

--- Javascript/hulkMod.js
+++ Javascript/hulkMod.js
@@ -1,13 +1,17 @@
-const body = Q('body');
-const hulkButton = Q('#activate-hulk-mod');
+hulkButton = document.querySelector('#activate-hulk-mod');
+body = document.querySelector('body');
+hulkForm= document.querySelector('#hulkForm');
 
-hulkButton.addEventListener('click', function() {
+hulkButton.addEventListener('click', function(e) {
+    e.preventDefault();
     body.classList.toggle('hulk-mod');
     if(body.className){
-        hulkButton.innerHTML = "AU SECOURS";
-        experience.innerHTML = "L'un des membres fondateurs du groupe de super-héros les Vengeurs.<br>Plus tard, rejoint l'équipe des Défenseurs.";
-        skills.innerHTML = "Sauts de 1000 mètres en hauteur, et d'une distance maximum de 6 kilomètres en longueur.<br>Ne crains ni les chaleurs extrêmes ni le froid, immunisé contre toutes les maladies terrestres.<br>Peut survivre dans le vide et le froid de l'espace.<br>Mes tissus se régénèrent à très grande vitesse.<br>Je suis inépuisable.<br>Soulever des montagnes, provoquer des tremblements de terre en frappant sur le sol ou provoquer des ondes de choc très puissantes en claquant des mains."
+        hulkButton.textContent = "AU SECOURS";
+        hulkForm.style.display="flex";
     } else {
-        hulkButton.innerHTML = "En savoir plus";
+        hulkButton.textContent = "En savoir plus";
+        hulkForm.style.display="none";
     }
+    //hulkForm.classList.toggle('display');
+    
 });
