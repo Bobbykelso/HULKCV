f69066167c8e9c40afb910c38f5fe9ad61a39914
Mariposa973 <75476880+Mariposa973@users.noreply.github.com>
2021-03-25 20:38:55 +0100

    Update footer.php
    class="imgIcons"

--- footer.php
+++ footer.php
@@ -7,23 +7,23 @@
 
 <div class="networkIcons">
     <a href="https://www.facebook.com/Hulk/" target="_blank">
-    <img src="public/img/icons/facebook.png" alt="facebookIcon" src="https://www.facebook.com/Hulk">
+    <img class="imgIcons" src="public/img/icons/facebook.png" alt="facebookIcon" src="https://www.facebook.com/Hulk">
     </a>
 
     <a href="https://www.instagram.com/hulk_brucebanner/" target="_blank">
-    <img src="public/img/icons/instagram.png" alt="instagramIcon" src="https://www.instagram.com/hulk_brucebanner">
+    <img class="imgIcons" src="public/img/icons/instagram.png" alt="instagramIcon" src="https://www.instagram.com/hulk_brucebanner">
     </a>
 
     <a href="https://www.pinterest.fr/TheeOneAboveAll/bruce-banner-hulk//" target="_blank">
-    <img src="public/img/icons/pinterest.png" alt="pinterestIcon" src="https://www.pinterest.fr/TheeOneAboveAll/bruce-banner-hulk/">
+    <img class="imgIcons" src="public/img/icons/pinterest.png" alt="pinterestIcon" src="https://www.pinterest.fr/TheeOneAboveAll/bruce-banner-hulk/">
     </a>
 
     <a href="https://twitter.com/Hulk/" target="_blank">
-    <img src="public/img/icons/twitter.png" alt="twitterIcon" src="https://twitter.com/Hulk">
+    <img class="imgIcons" src="public/img/icons/twitter.png" alt="twitterIcon" src="https://twitter.com/Hulk">
     </a>
 
     <a href="https://youtu.be/MmQdIgD5UVI" target="_blank">
-    <img src="public/img/icons/youtube.png" alt="youtubeIcon" src="https://youtu.be/MmQdIgD5UVI">
+    <img class="imgIcons" src="public/img/icons/youtube.png" alt="youtubeIcon" src="https://youtu.be/MmQdIgD5UVI">
     </a>
 </div>
 <div class="blankFooter">
